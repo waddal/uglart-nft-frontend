cd0d24067337f7f653b37ad7548b4a5b81be43de
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-11-07 15:57:09 -0800

    metamask auth implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  // Connecting MetaMask
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
       Connect to Wallet
     </button>
   );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="App">
@@ -25,7 +75,13 @@ function App() {
           <img src="#" alt="nft" />
         </div>
       </div>
-      {renderNotConnectedContainer()}
+      {currentAccount === "" ? (
+        renderNotConnectedContainer()
+      ) : (
+        <button onClick={null} className="cta-button connect-wallet-button">
+          Mint NFT
+        </button>
+      )}
     </div>
   );
 }
